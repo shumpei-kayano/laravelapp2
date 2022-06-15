305892cd8402cf6797e910ad548cdecc422c5075
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-06-15 12:35:31 +0900

    テキストP45まで

--- routes/web.php
+++ routes/web.php
@@ -18,30 +18,7 @@ return view('welcome');
         //　　↑resources/views/welcome.blade.phpのこと！
 });
 
-// Route::get('hello',function () {
-//     return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
-//  });
+Route::get('hello/{id?}/{pass?}', 'HelloController@index');
 
- Route::get('hello/{msg?}',function ($msg='no message') {
 
-    $html = <<<EOF
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-    body {font-size:16pt; color:#999; }
-    h1 { font-size:100pt; text-align:right; color:#eee;
-       margin:-40px 0px -50px 0px; }
-    </style>
-    </head>
-    <body>
-       <h1>Hello</h1>
-       <p>{$msg}</p>
-       <p>これは、サンプルで作ったページです。</p>
-    </body>
-    </html>
-    EOF;
-    
-       return $html;
-    });
     
