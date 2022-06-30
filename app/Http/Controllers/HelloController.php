81010ed2d08fe65f5d328c594710b49e99500d60
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-06-30 11:59:18 +0900

    クエリビルダ：insertによるレコード追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -29,13 +29,13 @@ class HelloController extends Controller
  
     public function create(Request $request)
     {
-        $param = [
-            'name' => $request->name,
-            'mail' => $request->mail,
-            'age' => $request->age,
-        ];
-        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
-        return redirect('/hello');
+       $param = [
+           'name' => $request->name,
+           'mail' => $request->mail,
+           'age' => $request->age,
+       ];
+       DB::table('people')->insert($param);
+       return redirect('/hello');
     }
 
     //---------データの編集----------
