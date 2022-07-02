c6e5c06c6b3590a0f439ee42e8c5d520f124feaf
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-07-02 11:19:04 +0900

    Revert "ORM:Personクラスにメソッドを追加する"
    This reverts commit c4036db87b3404a921e1eaebf08f916e458910f0.

--- app/Person.php
+++ app/Person.php
@@ -6,8 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    public function getData()
-    {
-        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
-    }
+    //
 }
