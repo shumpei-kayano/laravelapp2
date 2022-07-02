092cb7e382fe9e3a0b10cb3e560f76bc8f84fd7d
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-07-02 21:04:18 +0900

    ScopePersonクラスの作成

--- app/Person.php
+++ app/Person.php
@@ -5,23 +5,19 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
+    //グローバルスコープ
     protected static function boot()
     {
         parent::boot();
 
-        static::addGlobalScope('age', function(Builder $builder) {
-            $builder->where('age', '>', 20);
-        });
+        static::addGlobalScope(new ScopePerson);
     }
 
-    public function getData()
-    {
-        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
-    }
-
+    //ローカルスコープ
     public function scopeNameEqual($query, $str)
     {
         return $query->where('name', $str);
@@ -36,5 +32,12 @@ class Person extends Model
     {
         return $query->where('age', '<=', $n);
     }
+
+    public function getData()
+    {
+        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+    }
+
+
 
 }
