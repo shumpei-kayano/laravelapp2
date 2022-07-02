53c49eb9709538e542f6c076e03698ee40d155ed
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-07-02 11:31:22 +0900

    ORM：Personクラスにメソッドを追加する

--- app/Person.php
+++ app/Person.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    public function getData()
+    {
+        //test
+        return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
+    }
 }
