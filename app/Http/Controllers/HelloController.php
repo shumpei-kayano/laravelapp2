de57860047c32a0910f7929df1211bd91fb33296
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-07-03 11:17:05 +0900

    ページネーション：ソートの追加

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,9 +12,13 @@ class HelloController extends Controller
     //---------トップページ----------
     public function index(Request $request)
     {
-       $items = DB::table('people')->simplePaginate(5);
-       return view('hello.index', ['items' => $items]);
+       $sort = $request->sort;
+       $items = Person::orderBy($sort, 'asc')
+          ->simplePaginate(5);
+       $param = ['items' => $items, 'sort' => $sort];
+       return view('hello.index', $param);
     }
+    
  
     public function post(Request $request)
     {
