725a7459f7fabfcf6bcf3dbf591dfeb20b3369fb
Shumpei Kayano <shumpei.kayano@gmail.com>
2022-07-04 12:28:10 +0900

    P216クエリビルダ：ページネーション

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -11,7 +11,7 @@ class HelloController extends Controller
     //-----------トップページの表示-----------
     public function index(Request $request)
     {
-       $items = DB::table('people')->get();
+       $items = DB::table('people')->orderBy('age','asc')->get();
        return view('hello.index', ['items' => $items]);
     }
  
@@ -74,11 +74,11 @@ class HelloController extends Controller
 
     public function show(Request $request)
     {
-       $min = $request->min;
-       $max = $request->max;
+       $page = $request->page;
        $items = DB::table('people')
-           ->whereRaw('age >= ? and age <= ?',
-            [$min, $max])->get();
+           ->offset($page * 3)
+           ->limit(3)
+           ->get();
        return view('hello.show', ['items' => $items]);
     }
 }
