ab968acbe1967948018f9d610d9ac48b642192a0
shumpei-kayano <73801780+shumpei-kayano@users.noreply.github.com>
2022-07-06 11:51:32 +0900

    P246whereによる検索

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -20,8 +20,8 @@ class PersonController extends Controller
 
     public function search(Request $request)
     {
-        $item = Person::find($request->input);
-        $param = ['input' => $request->input, 'item' => $item];
-        return view('person.find', $param);
+       $item = Person::where('name', $request->input)->first();
+       $param = ['input' => $request->input, 'item' => $item];
+       return view('person.find', $param);
     }
 }
