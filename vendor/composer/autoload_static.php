73bd544ed4fc3e089184e494d5956054dfc35b8f
shumpei-kayano <73801780+shumpei-kayano@users.noreply.github.com>
2022-07-06 10:15:53 +0900

    P234マイグレーション、シーディング

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -3105,6 +3105,7 @@ class ComposerStaticInitf54aab18cccaf1f201a5314a187c8e55
         'PHPUnit\\Util\\Xml\\SuccessfulSchemaDetectionResult' => __DIR__ . '/..' . '/phpunit/phpunit/src/Util/Xml/SuccessfulSchemaDetectionResult.php',
         'PHPUnit\\Util\\Xml\\ValidationResult' => __DIR__ . '/..' . '/phpunit/phpunit/src/Util/Xml/ValidationResult.php',
         'PHPUnit\\Util\\Xml\\Validator' => __DIR__ . '/..' . '/phpunit/phpunit/src/Util/Xml/Validator.php',
+        'PeopleTableSeeder' => __DIR__ . '/../..' . '/database/seeds/PeopleTableSeeder.php',
         'PharIo\\Manifest\\Application' => __DIR__ . '/..' . '/phar-io/manifest/src/values/Application.php',
         'PharIo\\Manifest\\ApplicationName' => __DIR__ . '/..' . '/phar-io/manifest/src/values/ApplicationName.php',
         'PharIo\\Manifest\\Author' => __DIR__ . '/..' . '/phar-io/manifest/src/values/Author.php',
