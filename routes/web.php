9c4c4fdee7a5bd1d030b4bc573355e9695ae5b99
shumpei-kayano <73801780+shumpei-kayano@users.noreply.github.com>
2022-07-07 14:03:25 +0900

    Boardテーブルの作成

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,12 @@ Route::post('person/edit', 'PersonController@update');
 Route::get('person/del', 'PersonController@delete');
 Route::post('person/del', 'PersonController@remove');
 
+//-------------ORM(Board)--------------
+Route::get('board', 'BoardController@index');
+
+Route::get('board/add', 'BoardController@add');
+Route::post('board/add', 'BoardController@create');
+
 
 //----実習用-----
 Route::get('jissyu2', 'JissyuController@index');
