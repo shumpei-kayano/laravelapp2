08b91e49ce46e2a4c867df67d2639a19710ac038
shumpei-kayano <73801780+shumpei-kayano@users.noreply.github.com>
2022-07-10 09:58:28 +0900

    jissyu6_2

--- app/Http/Controllers/Jissyu6_1Controller.php
+++ app/Http/Controllers/Jissyu6_1Controller.php
@@ -10,15 +10,15 @@ class Jissyu6_1Controller extends Controller
     public function index(Request $request)
     {
         $items = Person::all();
-        $param = [
-            'items' => $items,
-            'input' => ''
-        ];
+        $param = ['input' => '','items' => $items];
         return view('jissyu6_1.index', $param);
     }
+
     public function find(Request $request)
     {
-        $item = Person::find($request->input);
+        //$item = Person::find($request->input);
+        $item = Person::where('name',$request->input)->first();
         return view('jissyu6_1.show', ['item' => $item]);
     }
+
 }
