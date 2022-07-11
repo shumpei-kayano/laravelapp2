157db303f1a47c699e86f6b89122b200bb722242
shumpei-kayano <73801780+shumpei-kayano@users.noreply.github.com>
2022-07-11 12:22:28 +0900

    ページネーション：simplePaginate

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,15 +5,24 @@ use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
 use Validator;
 use Illuminate\Support\Facades\DB;
+use App\Person;
 
 class HelloController extends Controller
 {
     //-----------トップページの表示-----------
     public function index(Request $request)
     {
-       $items = DB::table('people')->orderBy('age','asc')->get();
-       return view('hello.index', ['items' => $items]);
+        if(isset($request->sort)){
+            $sort = $request->sort;
+        }else {
+            $sort = 'age';
+        }
+       $items = Person::orderBy($sort, 'asc')
+          ->simplePaginate(5);
+       $param = ['items' => $items, 'sort' => $sort];
+       return view('hello.index', $param);
     }
+    
  
     public function post(HelloRequest $request)
     {
