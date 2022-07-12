0e48e166466d74f5be73e1ace23cba8f388c3e2e
shumpei-kayano <73801780+shumpei-kayano@users.noreply.github.com>
2022-07-12 11:21:05 +0900

    ユニットテスト

---
+++ tests/Feature/HelloTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+use App\Person;
+
+class HelloTest extends TestCase
+{
+    public function testHello()
+    {
+        // ダミーで利用するデータ
+        factory(User::class)->create([
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
+        factory(User::class, 10)->create();
+ 
+        $this->assertDatabaseHas('users', [
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
+ 
+        // ダミーで利用するデータ
+        factory(Person::class)->create([
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
+        factory(Person::class, 10)->create();
+ 
+        $this->assertDatabaseHas('people', [
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
+ 
+    }
+ 
+}
